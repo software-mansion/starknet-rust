e80f0d5dad8c1e4d30fadbc6aa3e4eac1f8daa19
ksew1 <karol.sewilo@swmansion.com>
2025-12-19 16:21:26 +0100

    Change node url

--- utils/test-common/src/lib.rs
+++ utils/test-common/src/lib.rs
@@ -2,9 +2,6 @@ use starknet_rust_providers::JsonRpcClient;
 use starknet_rust_providers::jsonrpc::HttpTransport;
 
 pub fn create_jsonrpc_client() -> JsonRpcClient<HttpTransport> {
-    let rpc_url = std::env::var("STARKNET_RPC")
-        .ok()
-        .filter(|var| !var.is_empty())
-        .unwrap_or_else(|| "https://api.zan.top/public/starknet-sepolia/rpc/v0_10".to_string());
-    JsonRpcClient::new(HttpTransport::new(url::Url::parse(&rpc_url).unwrap()))
+    let url = url::Url::parse("http://188.34.188.184:7070/rpc/v0_10").unwrap();
+    JsonRpcClient::new(HttpTransport::new(url))
 }
