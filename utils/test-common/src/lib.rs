f93c329c835a1115ee3bd1120ef752356d5a983b
Mohit Dhattarwal <48082542+Mohiiit@users.noreply.github.com>
2026-02-04 18:01:54 +0530

    fix: serialize shared-signer tests to eliminate nonce flakiness (#94)
    * tests: serialize shared-signer tx sends
    
    * ci: remove retry wrapper from tests

--- utils/test-common/src/lib.rs
+++ utils/test-common/src/lib.rs
@@ -1,7 +1,18 @@
 use starknet_rust_providers::JsonRpcClient;
 use starknet_rust_providers::jsonrpc::HttpTransport;
+use std::sync::OnceLock;
+use tokio::sync::{Mutex, MutexGuard};
 
 pub fn create_jsonrpc_client() -> JsonRpcClient<HttpTransport> {
     let url = url::Url::parse("http://188.34.188.184:7070/rpc/v0_10").unwrap();
     JsonRpcClient::new(HttpTransport::new(url))
+}
+
+static SHARED_SIGNER_LOCK: OnceLock<Mutex<()>> = OnceLock::new();
+
+pub async fn shared_signer_lock() -> MutexGuard<'static, ()> {
+    SHARED_SIGNER_LOCK
+        .get_or_init(|| Mutex::new(()))
+        .lock()
+        .await
 }
