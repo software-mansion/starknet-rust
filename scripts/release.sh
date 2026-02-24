f9eb5a6b34397cabe81e8768da647110bf6af5c2
Franciszek Job <54181625+franciszekjob@users.noreply.github.com>
2026-02-24 17:03:30 +0100

    Release script and guide (#110)
    * Add `release.sh`
    
    * Add `RELEASING.md`
    
    * Fix newline
    
    * Add `cargo check --workspace`
    
    * Apply review suggestion

---
+++ scripts/release.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -euxo pipefail
+
+VERSION=$1
+
+# Update CHANGELOG.md
+sed -i.bak "s/## \[Unreleased\]/## \[Unreleased\]\n\n## \[${VERSION}\] - $(TZ=Europe/Krakow date '+%Y-%m-%d')/" CHANGELOG.md
+rm CHANGELOG.md.bak 2> /dev/null
+
+# Update workspace version in Cargo.toml
+sed -i.bak "/\[workspace.package\]/,/version =/ s/version = \".*/version = \"${VERSION}\"/" Cargo.toml
+rm Cargo.toml.bak 2> /dev/null
+
+# Update dependencies versions in Cargo.toml
+sed -i.bak -e '/^\[workspace\.dependencies\]$/,/^\[/{' \
+           -e '/^[[:space:]]*starknet-rust[^[:space:]]*[[:space:]]*=/ s/version = "[^"]*"/version = "'"${VERSION}"'"/g' \
+           -e '}' Cargo.toml
+rm -f Cargo.toml.bak 2>/dev/null
+
+cargo check --workspace
