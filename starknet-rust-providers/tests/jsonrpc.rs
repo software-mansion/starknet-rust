7172d459ec4f11a217731d8434fc07a78b3f1ec3
Yaroslav Kukharuk <i.kukharuk@gmail.com>
2026-03-30 19:38:15 +0200

    fix: use hex-based storage key in jsonrpc_get_storage_proof test

--- starknet-rust-providers/tests/jsonrpc.rs
+++ starknet-rust-providers/tests/jsonrpc.rs
@@ -917,7 +917,7 @@ async fn jsonrpc_get_storage_proof() {
                     "04718f5a0fc34cc1af16a1cdee98ffb20c31f5cd61d6ab07201858f4287c938d",
                 )
                 .unwrap(),
-                storage_keys: vec![StorageKey("1".to_string())],
+                storage_keys: vec![StorageKey("0x1".to_string())],
             }],
         )
         .await
